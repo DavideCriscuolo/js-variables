f313e986e7e8dfb0a42a2691653900ad483f7626
DavideCriscuolo <davide.alcatel@gmail.com>
2025-05-26 16:07:40 +0200

    end 1 script

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -6,25 +6,37 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 // gli operatori numerici di Javascript
 
 // 1. Imposta questa variabile come somma di numOne e numTwo
-const numOnePlusNumTwo = NaN;
+const numOnePlusNumTwo = `La somma di ${numOne} e ${numTwo} è: ${
+  numOne + numTwo
+}`;
 console.log(numOnePlusNumTwo); // Output: 24
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
-const numThreeTimesNumTwo = NaN;
+const numThreeTimesNumTwo = `il prodotto di ${numThree} e ${numTwo} è: ${
+  numThree * numTwo
+}`;
 console.log(numThreeTimesNumTwo); // Output: 512
 
 // 3. Imposta questa variabile come divisione di numThree per numOne
-const numThreeDividedByNumOne = NaN;
+const numThreeDividedByNumOne = `La divisione di ${numThree} e ${numOne} è: ${
+  numThree / numOne
+}`;
 console.log(numThreeDividedByNumOne); // Output: 4
 
 // 4. Imposta questa variabile come differenza tra numThree e numOne
-const numThreeMinusNumOne = NaN;
+const numThreeMinusNumOne = `La differenza di ${numThree} e ${numOne} è: ${
+  numThree - numOne
+}`;
 console.log(numThreeMinusNumOne); // Output: 24
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = NaN;
+const sum = `La somma di ${numOne}, ${numTwo} e ${numThree}  è: ${
+  numOne + numTwo + numThree
+}`;
 console.log(sum); // Output: 56
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = NaN;
+const numBytes = `La somma di ${numOne}, ${numTwo} e ${numThree} diviso ${numOne} è: ${
+  (numOne + numTwo + numThree) / numOne
+}`;
 console.log(numBytes); // Output: 7
